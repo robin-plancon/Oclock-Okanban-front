b1a4b22c8c568e945208ea04cd4a515d4cbabe59
Robin <robin.plancon@outlook.com>
2023-07-26 12:04:24 +0200

    show/hide modal.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,38 @@
 var app = {
 
   // fonction d'initialisation, lancée au chargement de la page
-  init: function () {
+  init() {
     console.log('app.init !');
-  }
+    // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
+    app.addListenerToActions();
+  },
+
+  addListenerToActions() {
+    // on récupère le bouton d'ouverture de la modale
+    const modalButton = document.getElementById('addListButton');
+    // on accroche un écouteur d'évènement sur le bouton : quand on clique, on lance app.showAddListModal
+    modalButton.addEventListener('click', app.showAddListModal);
+
+    // on récupère les boutons de fermeture de la modale
+    const modalCloseButtons = document.querySelectorAll('.close');
+    // on parcourt les boutons de fermeture
+    modalCloseButtons.forEach( function(element) {
+      // on accroche un écouteur d'évènement sur le bouton : quand on clique, on lance app.hideModals
+      element.addEventListener('click', app.hideModals);
+    });
+  },
+
+  showAddListModal() {
+    const modal = document.getElementById('addListModal');
+    modal.classList.add('is-active');
+  },
+
+  hideModals() {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach( function(element) {
+      element.classList.remove('is-active');
+    });
+  },
 
 };
 
